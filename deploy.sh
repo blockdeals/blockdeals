f00aaf9c99f1660d0dffc7a362101cf9a24f3888
Scott Weston <scott@srw.id.au>
2018-06-29 15:45:10 +1000

    move blockdeals into the web network

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ git pull \
   && docker build -t blockdeals . \
   && docker stop blockdeals \
   && docker rm blockdeals \
-  && docker run -v /blockdeals.cfg:/blockdeals/blockdeals.cfg:ro --link=mongo:mongodb --name=blockdeals --restart=unless-stopped --net web -d blockdeals
+  && docker run -v /blockdeals.cfg:/blockdeals/blockdeals.cfg:ro --net web --link=mongo:mongodb --name=blockdeals --restart=unless-stopped -d blockdeals
